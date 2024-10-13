1ed3e3a39c0ba12a6f27f15232c0dcd44c8f78a9
LucaFasone <luca.fasone15@gmail.com>
2024-10-13 23:51:26 +0200

    updated gitmodules

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -11,7 +11,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     component: () => {
         return (
             <>
-
                 <Outlet />
             </>
         );
