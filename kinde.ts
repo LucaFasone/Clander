9e7432c8f298bdebd99fb46bf249277465add277
LucaFasone <luca.fasone15@gmail.com>
2024-10-15 01:47:11 +0200

    Update kinde.ts

--- kinde.ts
+++ kinde.ts
@@ -56,10 +56,6 @@ type Env = {
 export const getUser = createMiddleware<Env>(async (c, next) => {
   try {
     const isAuth = await kindeClient.isAuthenticated(sessionManager(c))
-    if (!isAuth) {
-      console.log('not auth');
-      return c.json({ error: "Unauthorized" }, 401);
-    }
     const user = await kindeClient.getUserProfile(sessionManager(c));
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     c.set("user", user);
